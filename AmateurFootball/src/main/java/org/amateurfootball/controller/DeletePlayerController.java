beed53cf9720359076191c57b0ee85d6d30710c5
czarq92 <czarq92@o2.pl>
2016-01-21 00:44:18 +0100

    Add DeletePlayerController

---
+++ AmateurFootball/src/main/java/org/amateurfootball/controller/DeletePlayerController.java
@@ -0,0 +1,37 @@
+package org.amateurfootball.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.amateurfootball.model.Player;
+import org.amateurfootball.model.Team;
+import org.amateurfootball.repository.TeamRepository;
+import org.amateurfootball.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DeletePlayerController {
+	
+	@Autowired
+	private TeamService teamService;
+	
+	@RequestMapping(value = "/delPlayer/{idPlayer}", method = RequestMethod.GET)
+	public String deletePlayer(@PathVariable("idPlayer") int idPlayer, Model model, HttpSession session){
+		
+		if( session.getAttribute("nameUserLogged") == null ){
+			return "redirect:/index";
+		}
+		
+		Long teamUserId = (Long) session.getAttribute("idTeamUserLogged");
+		
+		teamService.deletePlayerFromTeamPlayerList(teamUserId, idPlayer);
+		
+		return "redirect:/playerList";
+	}
+}
